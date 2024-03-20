7f7f9d697ab209aaf8b0f7a3af6026739fae1024
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-03-20 14:20:04 +0100

    Remove EvmAddress

--- src/config.template.ts
+++ src/config.template.ts
@@ -1,5 +1,6 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts"
 
+export const NETWORK_NAME = "{{network}}"
 export const UNISWAP_V3_QUOTER_V2_ADDRESS = Address.fromString("{{uniswapV3QuoterV2Address}}")
 export const WNATIVE_DECIMALS = BigInt.fromU32({{wrappedNativeDecimals}})
 export const CHAINLINK_NATIVE_PRICE_FEED_ADDRESS = Address.fromString("{{chainlinkNativePriceFeedAddress}}")
--- src/entity/transaction.ts
+++ src/entity/transaction.ts
@@ -1,6 +1,7 @@
 import { ethereum, log } from "@graphprotocol/graph-ts"
 import { Transaction } from "../../generated/schema"
 import { weiToBigDecimal } from "../utils/decimal"
+import { ADDRESS_ZERO } from "../utils/address"
 
 export function getTransaction(
   block: ethereum.Block,
@@ -13,7 +14,7 @@ export function getTransaction(
     tx = new Transaction(transactionId)
     tx.blockNumber = block.number
     tx.blockTimestamp = block.timestamp
-    tx.sender = transaction.from
+    tx.sender = transaction.from || ADDRESS_ZERO
     if (!receipt) {
       log.warning(
         'No receipt for transaction {}. Set "receipt: true" on the event handler configuration in subgraph.yaml',
