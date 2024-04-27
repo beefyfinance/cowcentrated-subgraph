8437009af647927f76b404e43674803116057e9b
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-04-27 12:39:12 +0100

    Remove unused values

--- src/utils/decimal.ts
+++ src/utils/decimal.ts
@@ -1,17 +1,8 @@
-import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts"
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
 export let TEN_BI = BigInt.fromI32(10)
-export let ZERO_BD = BigDecimal.fromString("0")
-export let ONE_BD = BigDecimal.fromString("1")
-export let TEN_BD = BigDecimal.fromString("10")
-export let EIGHTEEN_BI = BigInt.fromI32(18)
-export let ONE_ETH_BD = BigDecimal.fromString("1000000000000000000")
-export let ONE_ETH_BI = BigInt.fromString("1000000000000000000")
-export let ONE_GWEI_BI = BigInt.fromI32(1000000000)
-export let ONE_GWEI_BD = BigDecimal.fromString("1000000000")
-
 
 @inline
 export function changeValueEncoding(
@@ -30,7 +21,7 @@ export function changeValueEncoding(
 
 
 @inline
-export function exponentToBigInt(decimals: BigInt): BigInt {
+function exponentToBigInt(decimals: BigInt): BigInt {
   let bd = ONE_BI
   let n = decimals.toI32()
   for (let i = 0; i < n; i++) {
