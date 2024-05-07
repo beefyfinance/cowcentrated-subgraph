1667f9e307d5649f776dd4794d553a2034bc70aa
chebiN <17909998+seguido@users.noreply.github.com>
2024-05-07 15:03:00 -0300

    re-add dumb harvest event tracking for compounded fees calculations (#7)
    * re-add dumb harvest event tracking for compounded fees calculations
    
    * missing event import

--- src/mapping/strategy.ts
+++ src/mapping/strategy.ts
@@ -4,6 +4,7 @@ export {
   handleStrategyUnpaused as handleUnpaused,
 } from "../vault-lifecycle"
 export {
+  handleStrategyHarvest as handleHarvest,
   handleStrategyClaimedFees as handleClaimedFees,
   handleStrategyClaimedOutput as handleClaimedOutput,
 } from "../vault-compound"
